b2025f42b50c0a2935ba779c69373ae5e2e29d82
sam <mawenxia@foxmail.com>
2017-09-18 16:51:26 +0800

    生产者

---
+++ spring-cloud-01-provider/src/main/java/bhz/springcloud/ProviderApplication.java
@@ -0,0 +1,16 @@
+package bhz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient	//表示我是一个服务,注册到服务中心上
+@SpringBootApplication
+public class ProviderApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderApplication.class, args);
+	}
+	
+}
